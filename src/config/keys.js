79719610ffe8cb97eff1aa825967850fb9e2978e
Faizan Mehmood <faizanmehmood159@gmail.com>
2024-05-30 13:12:52 +0500

    cvff

--- src/config/keys.js
+++ src/config/keys.js
@@ -11,13 +11,7 @@ const config = {
   },
   JWT_SECRET: process.env.JWT_SECRET,
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-  // GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  // AWS: {
-  //   ACCESS_KEY: process.env.AWS_ACCESS_KEY,
-  //   SECRET_KEY: process.env.AWS_SECRET_KEY,
-  //   REGION: process.env.AWS_REGION,
-  //   BUCKET_NAME: process.env.S3_BUCKET_NAME
-  // }
+  
 };
 
 export default config;
